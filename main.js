4e133707517d0d8ec0af832322f27e4678d85884
Priya-cod <72642192+Priya-cod@users.noreply.github.com>
2021-05-29 13:44:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -48,6 +48,37 @@ function draw() {
     image(video, 0, 0, 600, 500);
     fill("#FF0000");
     stroke("#FF0000");
+    
+    if(scorerightWrist > 0.2)
+    {
+        circle(rightWristX, rightWristY, 20);
+
+        if(rightWristX >0 && rightWristY <= 100)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 0.5px";
+            song.rate(0.5);
+        }
+        else if(rightWristX >100 && rightWristY <= 200)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 1x";
+            song.rate(1);
+        }
+        else if(rightWristX >200 && rightWristY <= 300)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 1.5x";
+            song.rate(1.5);
+        }
+        else if(rightWristX >300 && rightWristY <= 400)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 2x";
+            song.rate(2);
+        }
+        else if(rightWristX >400 && rightWristY <= 500)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 2.5x";
+            song.rate(2.5);
+        }
+    }
 
     if(scoreleftWrist > 0.2)
     {
